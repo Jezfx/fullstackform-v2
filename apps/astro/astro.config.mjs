65b185145075f5294e5f88d0bdb6e83b969cea70
jez <jez@jezfx.com>
2025-07-16 22:36:33 +0100

    Update astro.config.mjs

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -12,5 +12,13 @@ export default defineConfig({
   adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
+    define: {
+      "process.env.NEXT_PUBLIC_SUPABASE_URL": JSON.stringify(
+        process.env.NEXT_PUBLIC_SUPABASE_URL
+      ),
+      "process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY": JSON.stringify(
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+      ),
+    },
   },
 });
